6ae9d5cb1249f4ab637013ec08ebed019d96c632
hhimansha <himansha.official@gmail.com>
2023-12-18 09:34:28 +0530

    book model

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,8 +1,21 @@
-import {PORT} from "./config.js";
+import {PORT, mongoDBURL} from "./config.js";
 import express from "express";
+import mongoose from "mongoose";
+//import nodemon from "nodemon";
 
 const app = express();
 
+app.get('/', (request,response) => {
+    console.log(request);
+    return response.status(234).send('Welcome to mern stack');
+});
+
 app.listen(PORT, () => {
     console.log(`App is listening on port: ${PORT}`);
+});
+
+mongoose.connect(mongoDBURL).then(() => {
+    console.log("Connected to the database");
+}).catch((error) => {
+    console.error("Error connecting to the database:", error);
 });
---
+++ BACKEND/models/bookModel.js
@@ -0,0 +1,23 @@
+import mongoose, { mongo } from "mongoose";
+
+const bookSchema = mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required : true,
+        }, 
+        author : {
+            type : String,
+            require : true,
+        },
+        publishYear : {
+            type : String,
+            require : true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+export const Book = mongoose.model('Cat', bookSchema);
