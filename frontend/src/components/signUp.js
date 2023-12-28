1d153aa2922ff96b8d64b65f56b62f88fe2febfd
hhimansha <himansha.official@gmail.com>
2023-12-28 12:40:25 +0530

    signup

--- frontend/src/components/signUp.js
+++ frontend/src/components/signUp.js
@@ -3,18 +3,22 @@ import { Link } from 'react-router-dom';
 
 function signUp(){
     return(
-        <div className="signupf orm">
-            <h1>Sign Up</h1>
-            <form className="w-70">
+        <div className="grid justify-center bg-primary mx-auto p-5 ">
+            <h1 className="text-4xl text-black text-center">Sign Up</h1>
+            <form className="w-96 grid">
                 <label>First Name</label>
-                <input type="text"></input>
+                <input type="text" className="rounded-full p-2 px-5"></input>
                 <label>Last Name</label>
-                <input type="text"></input>
+                <input type="text" className="rounded-full p-2 px-5"></input>
                 <label>Email Address*</label>
-                <input type="email"></input>
+                <input type="email" className="rounded-full p-2 px-5"></input>
                 <label>Password*</label>
-                <input type="password"></input>
-                <button type="button">Submit</button>
+                <input type="password" className="rounded-full p-2 px-5"></input>
+                <div className="btn section flex">
+                <button type="button" className="rounded-full p-2 px-5 bg-grey text-white p-4 m-2">Clear</button>
+                <button type="button" className="rounded-full p-2 px-5 bg-grey text-white p-4 m-2">Submit</button>
+                </div>
+                
             </form>
         </div>
     );
