863a29ed116185706c292bca2edf73daee6d7253
hhimansha <himansha.official@gmail.com>
2023-12-29 12:04:21 +0530

    modified login & signup

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -12,8 +12,15 @@ function login(){
                 <label className="text-grey">Password*</label>
                 <input type="password" className="rounded-full p-2 px-5 mb-4 border border-gray-300" placeholder="Enter your password here"></input>
                 <div className="btn section flex m-4 ml-0">
-                <button type="clear" className="px-5 text-20 text-black-600 font-semibold rounded-full border border-black hover:text-white hover:bg-grey hover:border-black">Clear</button>
-                <button type="submit" className="px-5 py-2 text-20 text-white font-semibold rounded-full border focus:outline-none bg-primary ml-3">Log In</button>
+                    <button
+                        type="submit"
+                        className="mx-auto px-5 py-2 px-10 text-20 text-white font-semibold rounded-full border focus:outline-none bg-primary"
+                        >
+                        Log In
+                    </button>
+                </div>
+                <div className="LoginLink text-center text-gray-500 m-4">
+                    New to Verbidise? <span className="text-primary">Sign up here</span> 
                 </div>
                 
             </form>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,11 +5,13 @@ module.exports = {
     extend: {
       backgroundColor: {
         'primary': '#ff8400',
-        'grey':'rgb(40, 40, 40)',
-        'grey-light' : 'rgb(250, 248, 245)',
+        'grey': 'rgb(40, 40, 40)',
+        'grey-light': 'rgb(250, 248, 245)',
+      },
+      textColor: {
+        'primary': '#ff8400', // Use the same color code as the 'primary' background color
       },
     },
   },
   plugins: [],
 }
-
