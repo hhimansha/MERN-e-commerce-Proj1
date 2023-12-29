132a1ce62cfc250f153c245da1a4f6b72df466e8
hhimansha <himansha.official@gmail.com>
2023-12-29 12:13:57 +0530

    modified signup

--- frontend/src/components/signUp.js
+++ frontend/src/components/signUp.js
@@ -6,13 +6,13 @@ function signUp(){
         <div className="grid justify-center bg-grey-light mx-auto my-20 p-5 rounded-3xl max-w-lg">
             <h1 className="text-4xl text-black text-center mb-4">Sign Up</h1>
             <form className="w-96 grid">
-                <label className="text-grey">First Name</label>
+                <label className="text-gray-500">First Name</label>
                 <input type="text" className="rounded-full p-2 px-5 mb-4 border border-gray-300" placeholder="Enter your first name here"></input>
-                <label className="text-grey">Last Name</label>
+                <label className="text-gray-500">Last Name</label>
                 <input type="text" className="rounded-full p-2 px-5 mb-4 border border-gray-300" placeholder="Enter your last name here"></input>
-                <label className="text-grey">Email Address*</label>
+                <label className="text-gray-500">Email Address*</label>
                 <input type="email" className="rounded-full p-2 px-5 mb-4 border border-gray-300" placeholder="Enter your email here"></input>
-                <label className="text-grey">Password*</label>
+                <label className="text-gray-500">Password*</label>
                 <input type="password" className="rounded-full p-2 px-5 mb-4 border border-gray-300" placeholder="Enter your password here"></input>
                 <div className="btn section flex m-4 ml-0">
                     <button
@@ -22,9 +22,9 @@ function signUp(){
                         Sign Up
                     </button>
                 </div>
-                <div className="LoginLink text-center text-gray-500 m-4">
-                    Already have an account? <span className="text-primary">Log in here</span> 
-                </div>
+                    <div className="LoginLink text-center text-gray-500 m-4">
+                        Already have an account? <span className="text-primary">Log in here</span> 
+                    </div>
                 
             </form>
         </div>
