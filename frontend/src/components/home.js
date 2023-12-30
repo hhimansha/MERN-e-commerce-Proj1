6ba421aa15154209eb2e701abc30e5b0c5940468
hhimansha <himansha.official@gmail.com>
2023-12-30 23:11:52 +0530

    frontedn

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -19,19 +19,20 @@ function Home() {
     return (
         <>
             <img src={img2} alt="Logo" className="max-w-full" />
+            <div className="fetchAllBooks flex">
             <div className="books text-black">
                 {books && books.map((book) => (
-                    <div key={book._id} className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                    <div className="max-w-sm bg-grey rounded-lg shadow dark:bg-grey-light">
                         <a href="#">
                             <img src={book.imageSrc} alt={book.title} className="rounded-t-lg" />
                         </a>
                         <div className="p-5">
                             <a href="#">
-                                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{book.title}</h5>
+                                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-grey">{book.title}</h5>
                             </a>
-                            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{book.description}</p>
+                            <p className="mb-3 font-normal text-gray-700 dark:text-gray-500">{book.description}</p>
                             <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                {book.price}
+                                {book.price}$
                                 <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
                                 </svg>
@@ -39,6 +40,7 @@ function Home() {
                         </div>
                     </div>
                 ))}
+            </div>
             </div>
         </>
     );
