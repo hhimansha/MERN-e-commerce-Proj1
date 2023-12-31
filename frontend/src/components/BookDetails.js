c692fd40d17fdc22d6cfdd51808c881224859725
hhimansha <himansha.official@gmail.com>
2023-12-31 12:50:32 +0530

    cart design

--- frontend/src/components/BookDetails.js
+++ frontend/src/components/BookDetails.js
@@ -10,13 +10,18 @@ const BookDetails = ({ book }) => {
                     <a href="#">
                     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-grey">{book.title}</h5>
                     </a>
-                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-500">{book.description}</p>
-                    <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-primary rounded-lg hover:bg-primary focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-primary dark:hover:bg-primary dark:focus:ring-blue-800">
-                    {book.price}$
-                    <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-                    </svg>
-                    </a>
+                    <p className="mb-1 font-normal text-gray-700 dark:text-gray-500">{book.author}</p>
+                    <p className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-grey">{book.price}$</p>
+                    <div className="btnSection grid items-center">
+                        <a href="#" className="px-5 py-2 text-20 text-black-600 font-semibold rounded-full border border-black hover:text-white hover:bg-grey hover:border-black text-center">
+                        Add to cart
+                        </a>
+                        <a href="#" className="px-5 py-2 text-20 text-white font-semibold rounded-full border focus:outline-none bg-primary text-center mt-2">
+                        Buy now
+
+                        </a>
+                    </div>
+                    
                 </div>
                 </div>
             </div>
