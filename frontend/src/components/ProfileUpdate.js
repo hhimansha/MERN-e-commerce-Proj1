17086c15db3f11ea1a3499d0df8f7d0853e00df2
hhimansha <himansha.official@gmail.com>
2024-01-29 12:46:27 +0530

    password update done

--- frontend/src/components/ProfileUpdate.js
+++ frontend/src/components/ProfileUpdate.js
@@ -1,26 +1,34 @@
 import { useAuthContext } from "../hooks/useAuthContext"
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const ProfileUpdate = () => {
     const { user, dispatch } = useAuthContext();
-    const [firstname, setFName] = useState(user.firstname || "");
-    const [lastname, setLName] = useState(user.lastname || "");
-    const [email, setEmail] = useState(user.email || "");
-    const [password, setPassword] = useState(""); // Initialize to an empty string
-    const [error, setError] = useState(null);
-  
-    // Set the initial password value when the component mounts
+  const [firstname, setFName] = useState(user.firstname || "");
+  const [lastname, setLName] = useState(user.lastname || "");
+  const [email, setEmail] = useState(user.email || "");
+  const [password, setPassword] = useState(""); // Initialize to an empty string
+  const [confirmPassword, setConfirmPassword] = useState(""); // Initialize to an empty string
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  // Set the initial password value when the component mounts
   useEffect(() => {
     // You can set a default value or keep it as an empty string
-    setPassword(user.password || ""); 
+    setPassword(user.password || "");
   }, [user]); // Trigger when the user data changes
 
-  
   const handleSubmit = async (e) => {
     e.preventDefault();
-  
-    const updatedUser = { firstname, lastname, email };
-  
+
+    // Check if the new password and confirm password match
+    if (password !== confirmPassword) {
+        setError("Passwords do not match");
+      return;
+    }
+
+    const updatedUser = { firstname, lastname, email};
+
     const response = await fetch(
       `http://localhost:9092/api/users/user/update/${user._id}`,
       {
@@ -32,16 +40,18 @@ const ProfileUpdate = () => {
         body: JSON.stringify(updatedUser),
       }
     );
-  
+
     const json = await response.json();
-    console.log('Backend Response:', json);
-  
+    console.log("Backend Response:", json);
+
     if (!response.ok) {
       setError(json.error);
     }
-  
+
     if (response.ok) {
-      dispatch({ type: 'UPDATE_USER', payload: json });
+      dispatch({ type: "UPDATE_USER", payload: json });
+      navigate("/user")
+      alert("Profile updated!")
     }
   };
   
@@ -61,10 +71,18 @@ const ProfileUpdate = () => {
                 <input
                     type="password"
                     className="rounded-full p-2 px-5 mb-4 border border-gray-300"
-                    placeholder="Enter your password here"
+                    placeholder="Enter your new password"
                     onChange={(e) => setPassword(e.target.value)}
                     value={password}
-                    />            
+                    />  
+                <input
+                    type="password"
+                    className="rounded-full p-2 px-5 mb-4 border border-gray-300"
+                    placeholder="Re-enter your new password"
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                     value={confirmPassword}
+                />  
+                              
                 <div className="btn section flex m-4 ml-0">
                     <button
                         type="submit"
@@ -73,6 +91,11 @@ const ProfileUpdate = () => {
                         Update
                     </button>
                 </div>
+                {error && (
+                <div className="text-red text-center m-2 text-2xl">
+                    {error}
+                </div>
+                )}
                 
             </form>
         </div>
