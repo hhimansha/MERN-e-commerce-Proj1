2665c18b76e4925a1af2513cd52a369accb9c55c
hhimansha <himansha.official@gmail.com>
2024-02-01 17:58:22 +0530

    empty cart

--- frontend/src/components/headerPart.js
+++ frontend/src/components/headerPart.js
@@ -56,16 +56,16 @@ function HeaderPart() {
         >
            <ul className="items-center flex flex-col p-4 md:p-0 mt-4 mx-2 font-medium -100 rounded-lg bg-grey md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-grey dark:bg-grey md:dark:bg-grey ">
             <li>
-                <a href="#" className="block py-2 px-3 text-gray-900 rounded  md:hover:text-primary md:p-0 md:dark:hover:text-primary dark:text-white  dark:hover:text-white dark:border-gray-700">Home</a>
+                <a href="/" className="block py-2 px-3 text-gray-900 rounded  md:hover:text-primary md:p-0 md:dark:hover:text-primary dark:text-white  dark:hover:text-white dark:border-gray-700">Home</a>
             </li>
             <li>
-                <a href="#" className="block py-2 px-3 text-gray-900 rounded md:hover:text-primary md:p-0 md:dark:hover:text-primary dark:text-white  dark:hover:text-white ">Categories</a>
+                <a href="/" className="block py-2 px-3 text-gray-900 rounded md:hover:text-primary md:p-0 md:dark:hover:text-primary dark:text-white  dark:hover:text-white ">Categories</a>
             </li>
             <li>
-                <a href="#" className="block py-2 px-3 text-gray-900 rounded   md:hover:text-primary md:p-0 md:dark:hover:text-primary dark:text-white dark:hover:text-primary  ">About</a>
+                <a href="/" className="block py-2 px-3 text-gray-900 rounded   md:hover:text-primary md:p-0 md:dark:hover:text-primary dark:text-white dark:hover:text-primary  ">About</a>
             </li>
             <li>
-                <a href="#" className="block py-2 px-3 text-gray-900 rounded  md:hover:text-primary md:p-0 md:dark:hover:text-primary dark:text-white  ">Contact</a>
+                <a href="/" className="block py-2 px-3 text-gray-900 rounded  md:hover:text-primary md:p-0 md:dark:hover:text-primary dark:text-white  ">Contact</a>
             </li>
             <li>
             <div className="relative">
